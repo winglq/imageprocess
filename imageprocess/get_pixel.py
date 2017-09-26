2136d62652281837885531486639eda6a20a8a62
Liu Qing <liuqing@chinac.com>
2017-09-26 19:08:48 +0800

    init
    Signed-off-by: Liu Qing <liuqing@chinac.com>

---
+++ imageprocess/get_pixel.py
@@ -0,0 +1,19 @@
+import numpy as np
+import sys
+
+from PIL import Image
+
+
+def get_pixel(path):
+    f = Image.open(path)
+    f2 = f.convert('L')
+    im_array = np.array(f2)
+    np.savetxt("pixels.csv", im_array, delimiter=",", fmt='%s')
+
+
+def main():
+    get_pixel(sys.argv[1])
+
+
+if __name__ == "__main__":
+    main()
